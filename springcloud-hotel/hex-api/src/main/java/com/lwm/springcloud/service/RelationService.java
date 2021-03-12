e57d6f5520578c1917d1f18bd692deda929f1593
unknown <jasonlulu0806@gmail.com>
2021-03-12 19:15:52 +0800

    second modify

--- springcloud-hotel/hex-api/src/main/java/com/lwm/springcloud/service/RelationService.java
+++
@@ -1,7 +0,0 @@
-package com.lwm.springcloud.service;
-import com.lwm.springcloud.entities.*;
-import java.util.List;
-public interface RelationService {
-	List<RelationKey> getFriends(int own);
-	int addFriend(int own ,int friend);
-}
--- springcloud-hotel/hex-api/src/main/java/com/lwm/springcloud/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.lwm.springcloud.service;
-import com.lwm.springcloud.entities.*;
-
-public interface UserService {
-	int addUser(User user);
-	User getUser(int accountNum);
-	void update(User user);
-}
